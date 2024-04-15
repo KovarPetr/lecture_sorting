5c57e2c0c73e7edce3069aa14bc52da700ea9a90
Kovář Petr (257014) <257014@vutbr.cz>
2024-04-15 13:29:11 +0200

    Insertion sorting

--- sorting.py
+++ sorting.py
@@ -69,9 +69,20 @@ def bubble_sort(list_to_sort):
     return list_to_sort
 
 
+def insertion_sort(list_to_sort):
+    n = len(list_to_sort)
+    for i in range(1, n):
+        key_number = list_to_sort[i]
+        j = i - 1
+        while j >= 0 and list_to_sort[j] > key_number:
+            list_to_sort[j + 1] = list_to_sort[j]
+            j= j - 1
+        list_to_sort[j + 1] = key_number
+    return list_to_sort
+
 def main():
     data = read_data("numbers.csv")
-    print(bubble_sort(data["series_1"]))
+    print(insertion_sort(data["series_1"]))
     pass
 
 

435e147da29683c22c31896ae553ccf5026a8dd3
Kovář Petr (257014) <257014@vutbr.cz>
2024-04-15 12:21:32 +0200

    Selection sort

--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 
 def read_data(file_name):
     """
@@ -10,9 +10,40 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    iteration = 0
+    with open(file_path, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = {}
+        for row in reader:
+            for key, value in row.items():
+                if iteration == 0:
+                    data[key] = [int(value)]
+                else:
+                    data[key].append(int(value))
+            iteration = iteration + 1
+        return data
+
+
+def selection_sort(list_to_sort):
+    iteration = 0
+    position = 0
+    for number in list_to_sort:
+        min = list_to_sort[iteration]
+        for comparable in list_to_sort[iteration:]:
+            if min > comparable:
+                min = comparable
+                position_min = position
+            position = position + 1
+        list_to_sort[iteration], list_to_sort[position_min] = list_to_sort[position_min], list_to_sort[iteration]
+        iteration = iteration + 1
+        position_min = iteration
+        position = iteration
+    return list_to_sort
 
 
 def main():
+    data = read_data("numbers.csv")
+    print(selection_sort(data["series_1"]))
     pass
 
 
